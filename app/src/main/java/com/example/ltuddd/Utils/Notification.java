271f57087b0d85fccc375c76242d846639bfce3f
LvNammm <luongvannam1308@gmail.com>
2023-04-07 22:04:33 +0700

    update somthing

--- app/src/main/java/com/example/ltuddd/Utils/Notification.java
+++ app/src/main/java/com/example/ltuddd/Utils/Notification.java
@@ -11,17 +11,17 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Notification {
-    public static void create(Context c,int id, String title, String text, Object systemService, boolean repeat, Date date, Integer hour, Integer minute, Integer second){
+    public static void create(Context c,int code, String title, String text, Object systemService, boolean repeat, Date date, Integer hour, Integer minute, Integer second){
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, hour);
         calendar.set(Calendar.MINUTE, minute);
         calendar.set(Calendar.SECOND, second);
 
         Intent intent = new Intent(c, NotificationReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(c, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(c, code, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         intent.putExtra("title",title);
         intent.putExtra("text", text);
-        intent.putExtra("id",id);
+        intent.putExtra("id",code);
         AlarmManager alarmManager = (AlarmManager) systemService;
         if(!repeat){
             Long time = date.getTime();
